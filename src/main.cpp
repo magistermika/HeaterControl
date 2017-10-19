dbb6e1fc07785093c40108877d32fbace4208823
Mika <mihaiilie@gmail.com>
2017-10-19 23:00:54 +0300

    bla

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <WEMOS_DHT12.h>
 #include <ESP8266WiFi.h>
-
+#include <settings.h>
 #include <Wire.h>
 
 //change wire pins because relay will be on D1
@@ -9,17 +9,40 @@ const int sdaPin = D2;
 
 const int relayPin = D1;
 const int pollInterval = 5000;
+const int desiredTemp = 24;
 
-const char* ssid = "ssidname";
-const char* password = "password";
- 
+const char* ssid = _ssid;
+const char* password = _password;
+
+
 int ledPin = BUILTIN_LED;
 
 unsigned long counter = 1;
 unsigned long prevMillis = 0;
 unsigned long currentMillis = 0;
+float temp = 0;
+float humi = 0;
+
+class Heater {
+public:
+    Heater();
+    void heaterMode(const char* mode);
+    float getTemp();
+    float getHumi();
+    void refresh();
+    void setDesiredTemp(float temp);
+    void heaterOn(bool on);
+    bool heaterState();
+
+private:
+    int _temp;
+    int _humi;
+    bool _isHeaterOn;
+};
+
 WiFiServer server(80);
 DHT12 dht12;
+Heater heater;
 
 void setup() {
   pinMode(relayPin, OUTPUT);
@@ -28,7 +51,6 @@ void setup() {
   Wire.begin(sdaPin, sclPin);
   Serial.begin(115200);
   delay(10);
- 
  
   digitalWrite(ledPin, HIGH);
  
@@ -59,20 +81,12 @@ void setup() {
 }
  
 void loop() {
-  //poll the senzor every 5000 seconds
+  //poll the sensor every 5000 seconds
   currentMillis = millis();
   if (currentMillis - prevMillis > pollInterval) {
     prevMillis = currentMillis;
-    if (dht12.get() == 0 && counter++) {
-      if (dht12.cTemp > 27) {
-        digitalWrite(ledPin, LOW);
-        digitalWrite(relayPin, HIGH);
-      }
-      else {
-        digitalWrite(ledPin, HIGH);
-        digitalWrite(relayPin, LOW);
-      }
-    }  
+    heater.refresh();    
+    counter++;
   }  
   // Check if a client has connected
   WiFiClient client = server.available();
@@ -93,17 +107,14 @@ void loop() {
   client.flush();
  
   // Match the request
- 
-  int value = LOW;
-  if (request.indexOf("/LED=ON") != -1) {
-    digitalWrite(ledPin, LOW);
-    digitalWrite(relayPin, HIGH);
-    value = HIGH;
+  if (request.indexOf("/Heater=ON") != -1) {
+    heater.heaterMode("manualOn");
   } 
-  if (request.indexOf("/LED=OFF") != -1){
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(relayPin, LOW);
-    value = LOW;
+  if (request.indexOf("/Heater=AUTO") != -1) {
+    heater.heaterMode("automode");
+  }
+  else {
+    heater.heaterMode("manualOff");
   }
  
   // Return the response
@@ -113,43 +124,78 @@ void loop() {
   client.println("<!DOCTYPE HTML>");
   client.println("<html>");
  
-  client.print("Led pin is now: ");
- 
-  if(value == LOW) {
-    client.print("Off<br>");  
+  client.print("Heater is now ");
+  client.print(heater.heaterState());
+  if(heater.heaterState() == false) {
+    client.print(" Off<br>");
   } else {
-    client.print("On<br>");
+    client.print(" On<br>");
   }
+    
+  client.println("<br><br>");
+  client.println("Click <a href=\"/Heater=ON\">here to turn heater ON</a><br>");
+  client.println("Click <a href=\"/\">here to turn heater OFF</a><br>");
+  client.println("Click <a href=\"/Heater=AUTO\">here to set to AUTO mode</a><br>");
   
-  client.print("Relay is now: ");
-  if(value == LOW) {
-    client.print("OFF<br>");  
-  } else {
-    client.print("ON<br>");
-  }
+  client.println("============<br> Temperature Control <br>============");
+  client.println("<br><br>");
 
-  
-  client.println("<br><br>");
-  client.println("Click <a href=\"/LED=ON\">here</a> turn the LED   ON<br>");
-  client.println("Click <a href=\"/LED=OFF\">here</a> turn the LED   OFF<br>");
-  client.println(dht12.get());
-  client.println("======= Cel mai boss de server cu temperatura/umiditate =======");
-  client.println("<br><br>");
-//   if(dht12.get()==0) {
-    client.println("Temperature in Celsius : ");
-    client.println(dht12.cTemp);
+  client.println("Temperature in Celsius : ");
+    client.println(heater.getTemp());
     client.println("<br>");
     client.print("Relative Humidity : ");
-    client.println(dht12.humidity);
+    client.println(heater.getHumi());
     client.println("<br>END");
     client.println(counter);
- // }
+
   
   client.println("</html>");
  
   delay(1);
   Serial.println("Client disconnected");
   Serial.println("");
- 
 }
 
+void Heater::heaterMode(const char* mode) {
+  if (mode == "manualOn") {
+    heaterOn(true);
+    return;
+  }
+  if (mode == "manualOff") {
+    heaterOn(false);
+    return;
+  }
+  else {
+    if (dht12.cTemp < desiredTemp) {
+      heaterOn(true);
+    }
+    else {
+      heaterOn(false);
+    }
+    return;
+  }
+}
+
+void Heater::heaterOn(bool on) {
+  if (on) {
+    digitalWrite(ledPin, LOW);
+    digitalWrite(relayPin, HIGH);
+    _isHeaterOn = true;
+  }
+  else {
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(relayPin, LOW);
+    _isHeaterOn = false;
+  }
+}
+
+bool Heater::heaterState() {
+  return _isHeaterOn;
+}
+
+void Heater::refresh() {
+  if (dht12.get()) {
+    _temp = dht12.cTemp;
+    _humi = dht12.humidity;
+  }  
+}
